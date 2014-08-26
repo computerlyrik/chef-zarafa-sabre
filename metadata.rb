0e19afb2a1e006e989bc5b7ed47251d3d3d6f92d
Christian Fischer <sw-dev@computerlyrik.de>
2014-08-26 21:29:30 +0200

    itnitial

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'zarafa-sabredav'
+maintainer       'computerlyrik, Christian Fischer'
+maintainer_email 'chef-cookbooks@computerlyrik.de'
+license          'Apache 2.0'
+description      'Installs/Configures zarafa-sabredav'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
